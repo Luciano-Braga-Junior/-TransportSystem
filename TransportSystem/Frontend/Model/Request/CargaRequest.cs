df9327eebff86782218d22442762db0193c6588c
Luciano Braga Junior <luciano.b.junior@es.estudante.senai.br>
2024-03-27 19:43:20 -0300

    push: Adicionando Model com a classe contendo as informações que irao ser pegas do cliente, e criando pagina web para colher as informações.

---
+++ TransportSystem/Frontend/Model/Request/CargaRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Frontend.Model.Request
+{
+    public class CargaRequest
+    {
+        public double DistanciaEmKm { get; set; }
+        public double PesoEmKg { get; set; }
+        public double Largura { get; set; }
+        public double Altura { get; set; }
+        public double Comprimento { get; set; }
+    }
+}
